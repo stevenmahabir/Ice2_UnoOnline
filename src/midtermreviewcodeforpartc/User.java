32356d8ec8f324568c2e1a7cccfdf8c010ab0f50
Steven M <mahastev@sheridancollege.ca>
2025-06-11 01:15:25 -0400

    Update User.java

--- src/midtermreviewcodeforpartc/User.java
+++ src/midtermreviewcodeforpartc/User.java
@@ -9,6 +9,7 @@ package midtermreviewcodeforpartc;
  * A class that models a User for Uno Online.
  * The user has a name and a password.
  * @author dancye, 2019
+ * @author Steve, 2025
  */
 public class User 
 {
